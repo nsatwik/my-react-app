e81f14687384cc45341621028fb45cd83354c0ad
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2024-08-09 10:31:49 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World, This is Satwik!
+          Welcome to myworld.com
         </p>
         <a
           className="App-link"
